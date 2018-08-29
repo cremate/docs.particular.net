6a1f188ef267cb2c0e4105f6d324f7463f0dcf43
danielmarbach <daniel.marbach@openplace.net>
2018-08-29 13:42:32 +0200

    Snippet for usage

---
+++ Snippets/MSDependencyInjection/MSDependencyInjection_1/Usage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+class Usage
+{
+    EndpointConfiguration endpointConfiguration;
+
+    #region msdependencyinjection
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // register any services to IServiceCollection
+
+        endpointConfiguration.UseContainer<ServicesBuilder>(customizations =>
+        {
+            customizations.ExistingServices(services);
+        });
+    }
+    #endregion
+}
